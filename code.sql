17b91285122125524ce832e97eec58754687a4b3
ashk <112991493+okayash@users.noreply.github.com>
2024-11-10 18:18:56 -0600

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,1 +1,13 @@
-CREATE TABLE
+CREATE TABLE Fragrance (
+  name VARCHAR(80) PRIMARY KEY,
+  rating INT,
+  CREATOR NOT NULL;
+);
+
+CREATE TABLE Scentfamily (
+  PRIMARY KEY
+);
+
+CREATE TABLE Users (
+  username VARCHAR(45) PRIMARY KEY;
+);
