c5407ae167f17d9a9b084d4e10598435484b2bd7
hi <112991493+okayash@users.noreply.github.com>
2024-11-16 21:40:50 -0600

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,1 +1,15 @@
 import mysql.connector
+
+# insertion
+# perfume insertion
+# perfumer insertion
+
+# deletion
+
+# perfume deletion
+# perfumer deletion
+
+# modify
+
+# perfume modification
+# perfumer modification
