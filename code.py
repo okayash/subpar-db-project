9a31e1099bfd8a49576fa784dd362565c2bfdf20
hi <112991493+okayash@users.noreply.github.com>
2024-11-16 21:36:56 -0600

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,32 @@
 import SQL_Query as sq
 import time
 import os
+
+# connect details here
+
+# create user
+
+# 
+# login 
+def login():
+
+# display perfumes
+def display_perf():
+  print("display details")
+
+# insertion
+# perfume insertion
+# perfumer insertion
+
+# deletion
+
+# perfume deletion
+# perfumer deletion
+
+# modify
+
+# perfume modification
+# perfumer modification
+
+
+
