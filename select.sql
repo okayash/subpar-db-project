8d91d67c5517a7f3364e42a5ee6c2eab18feef11
ashley <akfkgx@umsystem.edu>
2024-11-17 15:02:57 -0600

    select

--- select.sql
+++ select.sql
@@ -1,2 +1,11 @@
 -- select --
-SELECT * FROM __;
+SELECT * FROM PerfumeLine;
+SELECT * FROM Fragrance;
+SELECT * FROM Scentfamily;
+SELECT * FROM Subfamily;
+SELECT * FROM Users;
+SELECT * FROM Perfume;
+SELECT * FROM Perfumer;
+SELECT * FROM Cologne;
+SELECT * FROM F_release_date;
+SELECT * FROM user_collection;
