956b2f0b03a4c6f61a7a04f38edf80a8606a56da
hi <112991493+okayash@users.noreply.github.com>
2024-11-17 15:07:36 -0600

    Create temp.sql

---
+++ temp.sql
@@ -0,0 +1,7 @@
+--sample data c n p--
+
+INSERT INTO PerfumeLine(pline, pyear)
+Values ('jhag', 2024);
+
+INSERT INTO Fragrance(fname, rating, creator, ftop, fheart,fbase,fline)
+VALUES('juliette', 8, 'roman ricci', 'a', 'b', 'c', 'jhag');
