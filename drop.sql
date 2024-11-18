15e6ed6a946de2f7c4ddc2c2fd91b3b393f7de98
ashley <akfkgx@umsystem.edu>
2024-11-18 12:17:20 -0600

    changed order

--- drop.sql
+++ drop.sql
@@ -1,8 +1,8 @@
 -- just a file for testing --
+DROP TABLE Perfumer;
 DROP TABLE Perfume_details;
 DROP TABLE F_release_date;
 DROP TABLE Cologne;
-DROP TABLE Perfumer;
 DROP TABLE Perfume;
 DROP TABLE Users;
 DROP TABLE Subfamily;
