5f14807cee4e7dea1b1079cd8c98460a8f4ed076
ashley <akfkgx@umsystem.edu>
2024-11-18 21:37:07 -0600

    testing file

---
+++ querytests.sql
@@ -0,0 +1,12 @@
+-- Testing queries --
+
+-- search perfumes , need to join frag and perf_details
+
+
+--  most commonly used notes
+
+-- avg rating by base note
+
+--  show perfumes in each line
+  
+-- avg rating
