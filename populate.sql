66d3f3c5e8c8ba5f98cb3ef0c16c3869905e2b2c
hi <112991493+okayash@users.noreply.github.com>
2024-11-24 20:22:50 -0600

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -111,3 +111,14 @@ VALUES('YSL', 'Yves St Laurent'),
 ('Victorias Secret', 'VS');
 
 -- Select
+
+-- select --
+SELECT * FROM PerfumeLine;
+SELECT * FROM Fragrance;
+SELECT * FROM Family;
+SELECT * FROM Users;
+SELECT * FROM Perfume;
+SELECT * FROM Cologne;
+SELECT * FROM F_release_date;
+SELECT * FROM Perfume_details;
+SELECT * FROM Perfumer;
